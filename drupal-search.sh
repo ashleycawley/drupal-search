423f3776d2fb7489d8436abc6dc17390abbb6c9d
Ashley Cawley <acawley@cloudabove.com>
2018-04-24 16:23:29 +0100

    changed double-quotes for single apostrophes and added * at end of exclude path

--- drupal-search.sh
+++ drupal-search.sh
@@ -12,6 +12,6 @@
 # 
 echo && echo "Running the following command for you..." && echo
 
-echo "find /home/ -iname "drupal.php" -type f -not -path "/home/*/mail/" > /home/swb/acawley/drupal-search/results-`date +%d-%m-%Y-%H:%M`.txt"
-find /home/ -iname "drupal.php" -type f -not -path "/home/*/mail/" > /home/swb/acawley/drupal-search/results-`date +%d-%m-%Y-%H:%M`.txt
+echo "find /home/ -iname 'drupal.php' -type f -not -path "/home/*/mail/*" > /home/swb/acawley/drupal-search/results-`date +%d-%m-%Y-%H:%M`.txt"
+find /home/ -iname 'drupal.php' -type f -not -path "/home/*/mail/*" > /home/swb/acawley/drupal-search/results-`date +%d-%m-%Y-%H:%M`.txt
 exit 0
