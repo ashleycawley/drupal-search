7ef2998e1de0606a207354325507b81d8f7f3ebd
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-24 16:14:56 +0100

    Added first code and notes

---
+++ drupal-search.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+### Note section
+#
+# find /home/ -iname "drupal.php" -type f | xargs grep 'const VERSION' | awk -F\' '{print $2}'
+#
+# find /home/ -iname "drupal.php" -type f -not -path "/home/*/mail/"
+###
+#
+
+
+# 
+echo && echo "Running the following command for you..." && echo
+
+echo "find /home/ -iname "drupal.php" -type f -not -path "/home/*/mail/" > /home/swb/acawley/drupal-search/results-`date +%d-%m-%Y-%H:%M`.txt"
+find /home/ -iname "drupal.php" -type f -not -path "/home/*/mail/" > /home/swb/acawley/drupal-search/results-`date +%d-%m-%Y-%H:%M`.txt
+exit 0
